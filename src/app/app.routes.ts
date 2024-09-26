8627f6a33c22dd3ff026d1489f30f84b1953dd20
thehamzakargin <hamzakargin@outlook.com>
2024-09-26 10:25:45 +0300

    identfy

---
+++ src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { NoTaskComponent } from './tasks/no-task/no-task.component';
+import { UserTasksComponent } from './users/user-tasks/user-tasks.component';
+import { TasksComponent } from './tasks/tasks.component';
+import { NewTaskComponent } from './tasks/new-task/new-task.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    component: NoTaskComponent,
+  },
+  {
+    path: 'user/:userId',
+    component: UserTasksComponent,
+    children: [
+      {
+        path: 'tasks',
+        component: TasksComponent,
+      },
+      {
+        path: 'task/new',
+        component: NewTaskComponent,
+      },
+    ],
+  },
+];
